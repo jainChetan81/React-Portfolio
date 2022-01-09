08d465ce61c331e4f3b16ca897f95c3ba11eb374
jainChetan81 <jain.cj.chetan@gmail.com>
2022-01-09 10:00:47 +0530

    some minor improvements

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,12 +10,6 @@ class MyDocument extends Document {
 						rel="stylesheet"
 						href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
 					/>
-					<link
-						href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;500;700&display=swap"
-						rel="stylesheet"
-					/>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 					<link rel="apple-touch-icon" href="/oo" />
 					<meta name="theme-color" content="#ffffff" />
 					<meta name="msapplication-TileColor" content="#2B5797" />
