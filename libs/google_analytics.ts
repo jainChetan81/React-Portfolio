9c85e8a3aa1c0a32b4671371e016c6456602ae76
jainChetan81 <jain.cj.chetan@gmail.com>
2022-01-13 14:56:34 +0530

    resolved google analytics error

--- libs/google_analytics.ts
+++ libs/google_analytics.ts
@@ -1,3 +1,8 @@
+declare global {
+	interface Window {
+		gtag: any;
+	}
+}
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
