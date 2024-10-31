17d7f694d01d4b77ef29395f23c98fa291629be1
jain_chetan_spinny <chetan.jain@spinny.com>
2024-10-31 20:34:58 +0530

    updated link to resume

--- src/pages/api/resume.ts
+++ src/pages/api/resume.ts
@@ -3,11 +3,11 @@ import fs from "node:fs/promises";
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse): Promise<void> => {
 	try {
-		const file = await fs.readFile("public/chetan-resume.pdf");
+		const file = await fs.readFile("public/CHETAN_OCTOBER_31.pdf");
 		if (!file) throw new Error("File not found");
 
 		res.setHeader("Content-Type", "application/pdf");
-		res.setHeader("Content-Disposition", "attachment; filename=chetan-resume.pdf");
+		res.setHeader("Content-Disposition", "attachment; filename=CHETAN_OCTOBER_31.pdf");
 		res.send(file);
 	} catch (error: any) {
 		res.status(500).send(error.message);
